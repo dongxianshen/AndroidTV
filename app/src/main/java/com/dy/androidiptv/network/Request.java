fcf7cfdfe7632e2655d28cd726142e8a1fc8319e
dongyan <452665592@qq.com>
2017-06-06 15:36:20 +0800

    这个是Launcher
    这个是Android TV Launcher

---
+++ app/src/main/java/com/dy/androidiptv/network/Request.java
@@ -0,0 +1,9 @@
+package com.dy.androidiptv.network;
+
+public class Request {
+	private static final int CONNECTTIMEOUT = 30000;
+	private static final int READTIMEOUT = 20000;
+    private static boolean d = true;
+	public static String request( String json,String requestType ) {return "";}
+	public static String requestIp(String MacAddress) {return "";}
+}
